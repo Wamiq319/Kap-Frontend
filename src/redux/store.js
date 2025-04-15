b6f971e0805e30a9b1b601d2eccd1d643a148b7f
Wamiq <wamiqzahid319@gmail.com>
2025-04-15 23:18:27 +0500

    Add language slice for managing language state and update store configuration

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import adminCrudReducer from "./slices/adminCrudSlice";
-import langReducer from "./slices/langSlice";
+import langReducer from "./slices/langgSlice";
 
 const store = configureStore({
   reducer: {
